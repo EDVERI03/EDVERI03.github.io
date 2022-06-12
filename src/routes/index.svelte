c0f0d0800346e2516e31756b51ac67a3dfc66401
EDVERI03 <89509976+EDVERI03@users.noreply.github.com>
2022-06-12 08:23:40 +0200

    Removed image references as they made 'npm run build' impossible

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,11 @@
-<script>
-
-</script>
-
-
 <div class="bg-slate-200 w-3/4 m-auto flex flex-col items-center text-center min-h-[93%]">
     <h1 class="my-6 font-bold uppercase text-2xl text-slate-500">Murder mystery</h1>
     <hr class="border-t-4 border-dashed border-slate-100 w-full">
     <p class="m-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et ultricies ipsum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed a ante eget tortor euismod scelerisque. Quisque ac cursus lacus, eget tempor lacus. Ut ac convallis dolor. Suspendisse vel dui eu libero ullamcorper consectetur eu et lacus. Nullam finibus ante sit amet luctus commodo. Nullam condimentum, elit sit amet laoreet porta, erat mi euismod orci, a tincidunt metus felis sed nisi. Etiam vel finibus velit. Fusce id odio a metus posuere ullamcorper. Donec quis dui ut tortor ullamcorper tristique vel id est.</p>
     <div class="mx-10 rounded-lg bg-slate-400 flex flex-none place-content-center w-fit overflow-x-scroll scrollable" >
-        <img alt="card1" src="src/lib/assets/Portrait1.png" class="w-48 h-64 bg-white rounded-lg m-6 hue-rotate-90">
-        <img alt="card2" src="src/lib/assets/Portrait2.png" class="w-48 h-64 bg-white rounded-lg my-6">
-        <img alt="card3" src="src/lib/assets/Portrait3.png" class="w-48 h-64 bg-white rounded-lg m-6 -hue-rotate-90">
+        <img alt="card1" src="" class="w-48 h-64 bg-white rounded-lg m-6 hue-rotate-90">
+        <img alt="card2" src="" class="w-48 h-64 bg-white rounded-lg my-6">
+        <img alt="card3" src="" class="w-48 h-64 bg-white rounded-lg m-6 -hue-rotate-90">
     </div>
     <a class="p-4 rounded-lg bg-slate-500 text-white uppercase h-2/3 self-center m-10 font-bold hover:bg-slate-600 leading-11 h-fit" href="/Board"> Start Game </a>
 </div>
