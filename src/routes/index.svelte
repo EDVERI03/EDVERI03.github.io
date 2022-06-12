b9952103b771cff83cc5c224e2225cbea5a1ddf5
EDVERI03 <89509976+EDVERI03@users.noreply.github.com>
2022-06-12 19:38:05 +0200

    Added 'overflow hidden' to div in main

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,22 +2,10 @@
     <h1 class="my-6 font-bold uppercase text-2xl text-slate-500">Murder mystery</h1>
     <hr class="border-t-4 border-dashed border-slate-100 w-full">
     <p class="m-6">Someone has died in the Graymound hospital, which does happen but not like this. This person was murdured. After a few hours of work you, the investigator of this case, has narrowed down the possible suspects to only three people. One of these people must be the killer, it is your job to find out who did it. <br> The game is only playable with a mouse or trackpad (touchcontrols does not work), you can read how the game works in the instructions tab or start the game which is in the board tab.</p>
-    <div class="mx-10 rounded-lg bg-slate-400 flex flex-none place-content-center w-fit overflow-x-scroll scrollable" >
+    <div class="mx-10 rounded-lg bg-slate-400 flex flex-none place-content-center w-fit overflow-hidden" >
         <img alt="card1" src="Portrait1.png" class="w-48 h-64 bg-white rounded-lg m-6 hue-rotate-90">
         <img alt="card2" src="Portrait2.png" class="w-48 h-64 bg-white rounded-lg my-6">
         <img alt="card3" src="Portrait3.png" class="w-48 h-64 bg-white rounded-lg m-6 -hue-rotate-90">
     </div>
     <a class="p-4 rounded-lg bg-slate-500 text-white uppercase h-2/3 self-center m-10 font-bold hover:bg-slate-600 leading-11 h-fit" href="/Board"> Start Game </a>
 </div>
-
-<style>
-    .scrollable {
-        -ms-overflow-style: none;
-        scrollbar-width: none;
-    }
-
-    .scrollable::-webkit-scrollbar {
-        display: none;
-    }
-
-</style>
